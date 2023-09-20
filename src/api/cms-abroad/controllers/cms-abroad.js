839b19a98db268eb36c82c02f27aaa7ccea9f085
unknown <pharsh2412@gmail.com>
2023-09-20 14:27:44 +0530

    added abroad cms

---
+++ src/api/cms-abroad/controllers/cms-abroad.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cms-abroad controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cms-abroad.cms-abroad');
---
+++ src/api/cms-abroad/routes/cms-abroad.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cms-abroad router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cms-abroad.cms-abroad');
